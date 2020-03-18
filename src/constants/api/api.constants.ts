2673e24b8ffeadc2845cb67f2336e62b4ceebe37
Jop Verhoeven <jop.verhoeven@gmail.com>
2020-03-18 15:50:08 +0100

    Updated api url

--- src/constants/api/api.constants.ts
+++ src/constants/api/api.constants.ts
@@ -4,8 +4,8 @@ export const API_SUMMONER_URL = API_URL + 'summoner?name=';
 export const API_CHAMPION_NAME_URL = API_URL + 'champion?id=';
 export const API_RANKED_URL = API_URL + 'ranked?id=';
 
-export const DATA_URL = 'http://ddragon.leagueoflegends.com/cdn/10.5.1/';
-export const DATA_URL_NO_VERSION = 'http://ddragon.leagueoflegends.com/cdn/';
+export const DATA_URL = 'https://ddragon.leagueoflegends.com/cdn/10.5.1/';
+export const DATA_URL_NO_VERSION = 'https://ddragon.leagueoflegends.com/cdn/';
 export const DATA_SUMMONER_ICON_URL = DATA_URL + 'img/profileicon/';
 export const DATA_CHAMPION_ICON_URL = DATA_URL + 'img/champion/';
 export const DATA_CHAMPION_LOADING_URL = DATA_URL_NO_VERSION + 'img/champion/loading/';
